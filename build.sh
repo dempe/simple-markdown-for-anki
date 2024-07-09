5cab0e97ed525d19ee337faee210a749f2f9e529
Christopher Dempewolf <chris@chrisdempewolf.com>
2024-07-09 01:43:12 -0600

    Add build script

---
+++ build.sh
@@ -0,0 +1,4 @@
+rm build.ankiaddon
+
+# https://ankiweb.net/shared/by-author/1844938046
+zip -r build.ankiaddon README.md markdown_converter.py __init__.py test.py config.json config.md markdown-logo.svg
